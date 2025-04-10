fc95ede234dde8073e11d553bbcab2a5b644fc34
willyamzhang <willyamzhang@yahoo.com>
2025-04-10 12:30:18 +0700

    Add clock_out API

--- app/controllers/api/v1/sleep_records_controller.rb
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SleepRecordsController < ApplicationController
+  before_action :find_latest_by_user, only: [:update]
 
   def create
     record = SleepRecord.new(sleep_record_params)
@@ -10,9 +11,27 @@ class Api::V1::SleepRecordsController < ApplicationController
     end
   end
 
+  def update
+    if @latest_record.nil?
+      render json: { error: 'Sleep record not found' }, status: :not_found
+      return
+    end
+
+    if @latest_record.update(clock_out: Time.now)
+      render json: { message: "Clocked Out successfully", records: @latest_record }, status: :ok
+    else
+      render json: { errors: @latest_record.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def sleep_record_params
     params.permit(:user_id)
+  end
+
+  def find_latest_by_user
+    # passing user_id as params[:id]
+    @latest_record = SleepRecord.find_by("user_id = ? and clock_out is null", params[:id])
   end
 end
