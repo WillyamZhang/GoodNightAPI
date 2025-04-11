a1f292e5a45f3ec97c5cc173d1b5e99f8357b3f8
willyamzhang <willyamzhang@yahoo.com>
2025-04-11 13:43:57 +0700

    add validation cannot_follow_self & combination of user_id and followed_id must be unique.

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,12 @@
 class Follow < ApplicationRecord
   belongs_to :user
   belongs_to :followed, class_name: 'User'
+
+  #  check the combination of user_id and followed_id must be unique.
+  validates :user_id, uniqueness: { scope: :followed_id, message: "already follows this user" }
+  validate :cannot_follow_self
+
+  def cannot_follow_self
+    errors.add(:followed_id, "can't follow your self") if user_id == followed_id
+  end
 end
